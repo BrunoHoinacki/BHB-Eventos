e79a21a0f47d53d4c30ab156c2d07d5a95b61e3f
Bruno hoinacki <brunohoinackib@gmail.com>
2024-01-14 03:27:27 -0300

    :recycle: refactor: Criando EventController e a View do Controller linkando o evento no layout e criando a rota do evento

--- routes/web.php
+++ routes/web.php
@@ -13,26 +13,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
+use App\Http\Controllers\EventController;
 
-    $nome = "Bruno";
-    $idade = 24;
-    $profissao = "Programadador";
-    $arr = [10, 20, 30, 40, 50];
-
-    $nomes  = ["João", "Maria", "José"];
-
-    return view(
-        'welcome',
-        [
-            'nome' => $nome,
-            'idade' => $idade,
-            'profissao' => $profissao,
-            'arr' => $arr,
-            'nomes' => $nomes
-        ]
-    );
-});
+Route::get('/', [EventController::class, 'index']);
+Route::get('/events/create', [EventController::class, 'create']);
 
 Route::get('/contatos', function () {
     return view('contact');
