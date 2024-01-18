5f69893c59fc3ee10655a296c581dc6e4ba4247d
Bruno hoinacki <brunohoinackib@gmail.com>
2024-01-18 10:10:29 -0300

    Litlle changes

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,12 +6,7 @@
 <h1>Esta é a página home</h1>
 
 
-<img src="/images/banner.jpeg" alt="banner">
-@if (10 > 15)
-    <p>Condiçao verdadeira</p>
-@endif
-
-<p>{{ $nome }}</p>
+<img src="/images/banner.jpeg" alt="banner">s
 
 @if ($nome == 'Pedro')
     <p>O nome é Pedro</p>
@@ -20,22 +15,5 @@
 @else
     <p>O nome não é Pedro</p>
 @endif
-
-@for ($i = 0; $i < count($arr); $i++)
-    <p>{{ $arr[$i] }}</p>
-    @if ($i == 2)
-        <p>O i é 2</p>
-    @endif
-@endfor
-
-@php
-    $name = 'Bruno';
-    echo $name;
-@endphp
-
-@foreach ($nomes as $nome)
-    <p>{{ $loop->index }}</p>
-    <p>{{ $nome }}</p>
-@endforeach
 
 @endsection
