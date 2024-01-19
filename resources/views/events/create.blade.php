2884a7def78f27009d049c19d9e782365b47310f
Bruno hoinacki <brunohoinackib@gmail.com>
2024-01-19 13:08:53 -0300

    :sparkles: feat: Página de criar evento(not-finished)

--- resources/views/events/create.blade.php
+++ resources/views/events/create.blade.php
@@ -4,6 +4,24 @@
 
 @section('content')
 
-<h1>Crie um evento</h1>
+<div id="event-create-container" class="col-md-6 offset-md-3">
+    <h1>Crie o seu evento</h1>
+    <form action="/events" method="POST">
+        <div class="form-group">
+            <label for="title">Evento:</label>
+            <input type="text" class="form-control" id="title" name="title" placeholder="Nome do evento">
+        </div>
+        <div class="form-group">
+            <label for="title">Cidade:</label>
+            <input type="text" class="form-control" id="city" name="city" placeholder="Local do evento">
+        </div><div class="form-group">
+            <label for="title">Descrição</label>
+            <input type="text" class="form-control" id="description" name="description" placeholder="Descrição do evento">
+        </div><div class="form-group">
+            <label for="title">Privacidade:</label>
+            <input type="text" class="form-control" id="private" name="private" placeholder="Privacidade do evento">
+        </div>
+    </form>
+</div>
 
 @endsection
