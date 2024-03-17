6b840d47a69536951fa4f7a11f4daab290a4428f
Bruno Hoinacki <brunohoinackib@gmail.com>
2024-03-17 06:36:01 -0300

    :sparkles: feat: Corrigindo entrada na edição do evento, criando tabela de eventos que usuario participa

--- resources/views/events/dashboard.blade.php
+++ resources/views/events/dashboard.blade.php
@@ -5,7 +5,7 @@
 @section('content')
 
     <div class="col-md-10 offset-md-1 dashboard-title-container">
-        <h1>Meus Eventos</h1>
+        <h1>Meus eventos</h1>
     </div>
     <div class="col-md-10 offset-md-1 dashboard-events-container">
         @if (count($events) > 0)
@@ -25,7 +25,8 @@
                             <td><a href="/events/{{ $event->id }}">{{ $event->title }}</a></td>
                             <td>{{ count($event->users) }}</td>
                             <td>
-                                <a href="/events/edit/{{ $event->id }}" class="btn btn-info edit-btn"><ion-icon name="create-outline"></ion-icon>
+                                <a href="/events/edit/{{ $event->id }}" class="btn btn-info edit-btn"><ion-icon
+                                        name="create-outline"></ion-icon>
                                     Editar</a>
                                 <form action="/events/{{ $event->id }}" method="POST">
                                     @csrf
@@ -40,6 +41,37 @@
             </table>
         @else
             <p>Você ainda não tem evento, <a href="/events/create">Criar evento</a></p>
+        @endif
+    </div>
+    <div class="col-md-10 offset-md-1 dashboard-title-container">
+        <h1>Eventos que estou participando</h1>
+    </div>
+    <div class="col-md-10 offset-md-1 dashboard-events-container">
+        @if (count($eventsAsParticipant) > 0)
+            <table class="table">
+                <thead>
+                    <tr>
+                        <th scope="col">#</th>
+                        <th scope="col">Nome</th>
+                        <th scope="col">Participantes</th>
+                        <th scope="col">Ações</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    @foreach ($eventsAsParticipant as $event)
+                        <tr>
+                            <td scope="row">{{ $loop->index + 1 }}</td>
+                            <td><a href="/events/{{ $event->id }}">{{ $event->title }}</a></td>
+                            <td>{{ count($event->users) }}</td>
+                            <td>
+                                <a href="#">Sair do evento</a>
+                            </td>
+                        </tr>
+                    @endforeach
+                </tbody>
+            </table>
+        @else
+            <p>Você não esta participando de nenhum evento, <a href="/">veja todos os eventos</a></p>
         @endif
     </div>
 
